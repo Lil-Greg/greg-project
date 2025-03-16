901659913a83342a5342561e4b00440e724eb9bc
Lil-Greg <gregorywilliams1289@gmail.com>
2025-03-15 23:51:21 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello, My name is Greg!</h1>
+      <p>My Name is Greg. I am 16 and I got to McKinley Tech HighSchool. I was born in Washington, D.C., USA</p>
+    </>
+  )
+}
+
+export default App
