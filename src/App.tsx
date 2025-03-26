d73e962958979515853b1660992285aaefd74b7a
Lil-Greg <gregorywilliams1289@gmail.com>
2025-03-25 22:33:19 -0400

    Added Login, Register, and Home Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './views/Home'
+import Login from './views/auth/Login'
+import Register from './views/auth/Register'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello, My name is Greg!</h1>
-      <p>My Name is Greg. I am 16 and I got to McKinley Tech HighSchool. I was born in Washington, D.C., USA</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/views/auth/Register.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+export default function Register() {
+    return (
+        <>
+            <Container className="container">
+                <h1>Register</h1>
+            </Container>
+        </>
+    )
+}
